7ea61305370b5f51de9521bc8c4f30dc704957f6
BaosenZ <260702560@qq.com>
2022-10-23 16:21:56 -0400

    section update

---
+++ section3_objectDetectionUsingJetsonNano/inferenceProFow/predImages.py
@@ -0,0 +1,17 @@
+import jetson.inference
+import jetson.utils
+import cv2
+import os
+import numpy as np
+
+net = jetson.inference.detectNet('ssd-mobilenet-v2',['--model=/home/boikalab/jetson-inference/python/training/detection/ssd/models/amoebaProFowModel_mb2/mb2-ssd-lite.onnx','--input_blob=input_0','--output_cvg=scores','--output_bbox=boxes','--labels=/home/boikalab/jetson-inference/python/training/detection/ssd/models/amoebaProFowModel_mb2/labels.txt','--threshold=0.2'])
+
+fileList = os.listdir('./testDataset/')
+for i in fileList:
+	img = cv2.imread("./testDataset/" + str(i))
+	img = jetson.utils.cudaFromNumpy(img)
+	detections=net.Detect(img)
+	img = np.ascontiguousarray(img)
+	ImgName = str(i)
+	cv2.imwrite(ImgName,img)
+
